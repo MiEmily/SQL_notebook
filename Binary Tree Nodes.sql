1a02022376b9e62c46c5d36e866fce49c959b5b9
Minrui Hu <120063655+MiEmily@users.noreply.github.com>
2023-09-12 18:31:14 -0700

    Create Binary Tree Nodes.sql

---
+++ Binary Tree Nodes.sql
@@ -0,0 +1,7 @@
+/*
+select 1 clause:
+  select 1/select * /select null common used in EXISTS subselect. In EXISTS subselect, the database does not actually "retrieve" rows,
+it doesn not always need to scan the entire result set for the sbselect, becasue just one row will provide an answer. That answer either TRUE or FALSE
+.
+
+*/
