479fb51bd42575796b2a7b8c16c50b98dfea9636
Minrui Hu <120063655+MiEmily@users.noreply.github.com>
2023-09-16 12:44:12 -0700

    Update Advanced_DB_System.sql

--- school_work/Advanced_DB_System.sql
+++ school_work/Advanced_DB_System.sql
@@ -1,3 +1,14 @@
 /*
  CREATE SCHEMA AUTHENTAION  (create a database schema) a schema: a collection of db objects like tables, triggers, stored procedures.
   */
+
+
+ /*
+
+ERD Design
+entity:represent object in databse. the name of every entity should be singular 
+attribute: the properties of an entity
+
+crow's foot notation : specify the relationship between one entity with another 
+cardinality acts as a parameter for the relationship between entities. For one entity, there is a minimum and maximum number that helps define its relationship with another entity.
+*/
