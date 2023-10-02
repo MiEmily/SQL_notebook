9852b210bad6d73c27ee8007c5ef035daf799b30
Minrui Hu <120063655+MiEmily@users.noreply.github.com>
2023-10-01 18:22:44 -0700

    Update 178. Rank Scores.sql

--- leetcode/178. Rank Scores.sql
+++ leetcode/178. Rank Scores.sql
@@ -1,1 +1,5 @@
+-- from the question: 2. tier,same ranking 3. no gap in ranks ----> DENSE_RANK()
+-- 1. score descending. 
+-- PARTITION BY optional, ORDER BY necessary.
+-- use ORDER BY for score
 
