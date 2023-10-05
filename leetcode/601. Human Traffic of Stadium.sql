bafaee4f7724abd2bac7375a1f04ca33595b55eb
Minrui Hu <120063655+MiEmily@users.noreply.github.com>
2023-10-05 12:35:25 -0700

    Update 601. Human Traffic of Stadium.sql

--- leetcode/601. Human Traffic of Stadium.sql
+++ leetcode/601. Human Traffic of Stadium.sql
@@ -1,1 +1,2 @@
-
+the perfect solution key take away
+1.it use id-row_number() to diff and then use group by diff check consecutive (it id consecutive, which means they like 1+n,2+n,3+n,4+n,(so it like row_number+n=id))
